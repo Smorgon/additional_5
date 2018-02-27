2bd69d2f8828b72c2efc536aa5e8dcd7ad9fa144
Mikhail Rohau <misha_rogov@tut.by>
2018-02-28 00:38:34 +0300

    Implementation function check

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let expression = bracketsConfig.map((v) => {
+        if (v.join('').replace(/[^\d]/g, '') != '') {
+            return "(" + v.join("") + ")";
+        }
+        return "(\\" + v.join("\\") + ')';
+    }).join('|');
+
+    var regExp = new RegExp(expression,"g");
+    let prevStr = '';
+    while (prevStr != str && str) {
+        prevStr = str;
+        str = str.replace(regExp, '');
+    }
+
+    return str === '';
 }
